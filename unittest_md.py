79f9c46c3d0b057f2a29a28e44e19ec00375bf8b
Hans-Ove Åkesson <143617150+hanak526@users.noreply.github.com>
2025-09-29 14:58:13 +0200

    Update unittest_md.py

--- unittest_md.py
+++ unittest_md.py
@@ -13,13 +13,15 @@ class MdTests(unittest.TestCase):
         pbc=True,
     )
         from ase.calculators.emt import EMT
-        atoms.calc = EMT()
-        value = [-0.045452090868632666, 0.0, 0.0, -0.045452090868632666]
-        self.assertEqual(value, calcenergy(atoms))
+        atoms.calc = EMT()value = atoms.get_potential_energy()/len(atoms)
+        real_value = calcenergy(atoms)
+        message = "It works!"
+        self.assertTrue(value==real_value[0],message)
 if __name__ == "__main__":
     tests = [unittest.TestLoader().loadTestsFromTestCase(MdTests)]
     testsuite = unittest.TestSuite(tests)
     result = unittest.TextTestRunner(verbosity=0).run(testsuite)
 
     sys.exit(not result.wasSuccessful())
+
 
